cf193e8b0272be36ca6f067dd881dcb7369c2c63
Jorge <jsamo25@gmail.com>
2020-09-30 20:36:41 +0200

    Parts 15, 16, 17, 18
    (class notes, not completed yet)

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pandas as pd
 
 pd.set_option("display.max_columns", 99)
+data = pd.read_csv("sentiment10.csv")
 
 #10, 11, 12, 13
-data = pd.read_csv("sentiment10.csv")
-print(data.shape)
-print(data[:3])
-print(data["sentiment"])
-print(data["sentiment"].unique())
+# print(data.shape)
+# print(data[:3])
+# print(data["sentiment"])
+# print(data["sentiment"].unique())
 
+#15, 16, 17, 18
+
+print(data["sentiment"]=="pos") #checks if each row matches the "pos" value
+
+#assign a boolean value to a pandas column
+data["sentiment"] = data["sentiment"] == "pos"
+print(data)
+
+data["n_characters"] = data["text"].apply(len)
+data["tookens"] = data["text"].apply(...)
