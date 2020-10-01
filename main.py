8d2d9a44e0a8446e5b14397d1cca943fb57d67cf
Jorge <jsamo25@gmail.com>
2020-10-01 18:59:13 +0200

    Adding column with tokenized text, updating requirement.txt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import nltk
 
 """ Getting Started """
 pd.set_option("display.max_columns", 99)
@@ -8,18 +9,21 @@ data = pd.read_csv("sentiment10.csv")
 def part_b():
     #10, 11, 12, 13
     print(data.shape)
-    print(data[:3])
-    print(data["sentiment"])
-    print(data["sentiment"].unique())
+    # print(data[:3])
+    # print(data["sentiment"])
+    # print(data["sentiment"].unique())
 
-    #15, 16, 17, 18
-    print(data["sentiment"]=="pos") #checks if each row matches the "pos" value
+    #15, 16
+    #print(data["sentiment"]=="pos") #checks if each row matches the "pos" value
     #assign a boolean value to a pandas column
     data["sentiment"] = data["sentiment"] == "pos"
-    print(data)
+    #print(data)
     data["sentiment"] = data["sentiment"].astype("bool")
+    #print(data)
+
+    #17, 18
+    data['tokens'] = data['text'].apply(nltk.tokenize.word_tokenize)
     print(data)
-    # data["n_characters"] = data["text"].apply(len)
     # data["tookens"] = data["text"].apply(...)
 
 part_b()
