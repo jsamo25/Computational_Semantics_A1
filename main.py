4f6ca519ef6b5f57f1e5259e6be5fadf5f2bc18a
Jorge <jsamo25@gmail.com>
2020-10-06 14:06:40 +0200

    Decided to remove the _function_ format still requiring a sequential/better presented code, but for now will continue with a _code block_ approach

--- main.py
+++ main.py
@@ -3,73 +3,87 @@ import numpy as np
 import nltk
 import matplotlib.pyplot as plt
 
-def part_a():
-    # TODO: make use of a TRIAL variable to switch between the different data sets (Part B).
-    pd.set_option("display.max_columns", 99)
-    data = pd.read_csv("sentiment10.csv")
-    #data = pd.read_csv("sentiment.csv")
-    #print(data["text"])
-    return data
+""" PART A """
+# TODO: make use of a TRIAL variable to switch between the different data sets (Part B).
+pd.set_option("display.max_columns", 99)
+data = pd.read_csv("sentiment10.csv")
+#data = pd.read_csv("sentiment.csv")
+#print(data["text"])
 
-def part_b():
-    #TODO: create better functions, maybe a menu?
-    data = part_a()
-    #10, 11, 12, 13
-    print(data.shape)
-    print(data[:3])
-    print(data["sentiment"])
-    print(data["sentiment"].unique())
+""" PART B"""
+#TODO: create better functions, maybe a menu?
+#FIXME: change commented prints before submitting
+#10, 11, 12, 13
+# print(data.shape)
+# print(data[:3])
+# print(data["sentiment"])
+# print(data["sentiment"].unique())
 
-    #15, 16
-    #print(data["sentiment"]=="pos") #checks if each row matches the "pos" value
-    data["sentiment"] = data["sentiment"] == "pos"
-    #print(data)
-    data["sentiment"] = data["sentiment"].astype("bool") #assign boolean values to sentiment column
-    #print(data)
+#15, 16
+#print(data["sentiment"]=="pos") #checks if each row matches the "pos" value
+data["sentiment"] = data["sentiment"] == "pos"
+#print(data)
+data["sentiment"] = data["sentiment"].astype("bool") #assign boolean values to sentiment column
+#print(data)
 
-    #17, 18, 19
-    data['n_characters'] = data['text'].apply(lambda x: len(x))
-    #data['n_characters'] = data['text'].str.len()
-    data['tokens'] = data['text'].apply(nltk.tokenize.word_tokenize)
-    data['n_tokens'] = data['tokens'].apply(lambda x: len(x))
-    #data['n_tokens'] = data['tokens'].str.len()
+#17, 18, 19
+data['n_characters'] = data['text'].apply(lambda x: len(x))
+#data['n_characters'] = data['text'].str.len()
+data['tokens'] = data['text'].apply(nltk.tokenize.word_tokenize)
+data['n_tokens'] = data['tokens'].apply(lambda x: len(x))
+#data['n_tokens'] = data['tokens'].str.len()
 
-    #print(data)
-    #21, mean, standard deviation
-    print("Mean Values\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].mean())
-    print("\n Standard deviation Values\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].std())
-    #print("Data Statistics", "\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].describe())
+#FIXME: change commented prints before submitting
+#21, mean, standard deviation
+# print("Mean Values\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].mean())
+# print("\n Standard deviation Values\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].std())
+#print("Data Statistics", "\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].describe())
 
-    #22 Histogram of the rating column fo the DataFrame sentiment.
-    plt.hist(data["rating"])
-    print("Data Description\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].describe())
-    plt.grid(True)
-    plt.show()
+#22 Histogram of the rating column fo the DataFrame sentiment.
+# plt.hist(data["rating"])
+# print("Data Description\n",data[["rating", "sentiment", "n_characters", "n_tokens"]].describe())
+# plt.grid(True)
+# plt.show()
 
-def part_c():
-    data = part_a()
-    #25The use of features may help to create functions, and later make predictions
-    #26 Times that appear the word bad, Times that appear the word good, capitalized words?, exclamation marks?
-    #27 selected feature: Times that appear the word "good"
+""" PART C """
+#25The use of features may help to create functions, and later make predictions
+#26 Times that appear the word bad, Times that appear the word good, capitalized words?, exclamation marks?
+#27 selected feature: Times that appear the word "good"
 #TODO: Find a better feature, "good" or "bad" wont appear all the time, default neutral? (what about negations? i.e not good, not bad. And Sarcasm?)
-    # def n_good(text):
-    #     computed_feature = text.count('good')
-    #     return computed_feature
-    #
-    # data['n_good'] = data['text'].apply(n_good)
-    # print(data["n_good"])
+
+# def n_good(text):
+#     computed_feature = text.count('good')
+#     return computed_feature
+#
+# data['n_good'] = data['text'].apply(n_good)
+# print(data["n_good"])
 
 #29 Number of positive & Number of negative words based on polarity lexicon. Figure 21.3 https://web.stanford.edu/~jurafsky/slp3/21.pdf
-    positive_lexicons = ["admire", "amazing", "assure", "celebration", "charm", "eager", "enthusiastic", "excellent",
-                         "fancy", "fantastic", "frolic", "graceful", "happy", "joy", "luck", "majesty", "mercy", "nice",
-                         "patience", "perfect", "proud", "rejoice", "relief", "respect", "satisfactorily", "sensational",
-                         "super", "terrific", "thank", "vivid", "wise", "wonderful", "zest", "good"]
-    negative_lexicons = ["abominable", "anger", "anxious", "bad", "catastrophe", "cheap", "complaint", "condescending",
-                         "deceit","defective", "disappointment", "embarrass", "fake", "fear", "filthy", "fool", "guilt",
-                         "hate", "idiot", "inflict", "lazy", "miserable", "mourn", "nervous", "objection", "pest", "plot",
-                         "reject", "scream", "silly", "terrible", "unfriendly","vile", "wicked"]
+positive_lexicons = ["admire", "amazing", "assure", "celebration", "charm", "eager", "enthusiastic", "excellent",
+                     "fancy", "fantastic", "frolic", "graceful", "happy", "joy", "luck", "majesty", "mercy", "nice",
+                     "patience", "perfect", "proud", "rejoice", "relief", "respect", "satisfactorily", "sensational",
+                     "super", "terrific", "thank", "vivid", "wise", "wonderful", "zest", "good"]
+negative_lexicons = ["abominable", "anger", "anxious", "bad", "catastrophe", "cheap", "complaint", "condescending",
+                     "deceit","defective", "disappointment", "embarrass", "fake", "fear", "filthy", "fool", "guilt",
+                     "hate", "idiot", "inflict", "lazy", "miserable", "mourn", "nervous", "objection", "pest", "plot",
+                     "reject", "scream", "silly", "terrible", "unfriendly","vile", "wicked"]
 
+def positive_lex_n(text):
+    computed_feature = 0
+    for word in text:
+        if word in positive_lexicons:
+            computed_feature += 1
+    return computed_feature
 
+data['positive_lex_n'] = data['tokens'].apply(positive_lex_n)
+print(data["positive_lex_n"])
 
+def negative_lex_n(text):
+    computed_feature = 0
+    for word in text:
+        if word in negative_lexicons:
+            computed_feature += 1
+    return computed_feature
 
-part_c()
+data['negative_lex_n'] = data['tokens'].apply(negative_lex_n)
+print(data["negative_lex_n"])
