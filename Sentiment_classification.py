f9c23749f6cf0ca42603c9e5401ba34973f0b3db
Jorge <jsamo25@gmail.com>
2020-10-09 15:34:21 +0200

    Initial commit

---
+++ Sentiment_classification.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import nltk
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+from pdb import set_trace
+
+"""Importing the dataset + target column boolean transformation """
+data = pd.read_csv("sentiment10.csv")
+data["sentiment"] = data["sentiment"] == "pos"
+data["sentiment"] = data["sentiment"].astype("bool") #assign boolean values to sentiment column
+
+"""PART J. Basic task and data explporation [max 2 points]"""
+#Report some basic statistics of the dataset (means, standard deviations). Also
+# report the average number of words per review and the average number of sentences per review.
+
+data['n_characters'] = data['text'].apply(lambda x: len(x))
+data["sentences"] = data["text"].apply(nltk.tokenize.sent_tokenize)
+data["n_sentences"] = data["sentences"].apply(lambda x: len(x))
+data['tokens'] = data['text'].apply(nltk.tokenize.word_tokenize)
+data['n_tokens'] = data['tokens'].apply(lambda x: len(x))
+
+#4
+pos_rev_word_avg = (data["n_characters"].groupby(data["sentiment"]==True)).mean()
+neg_rev_word_avg = (data["n_characters"].groupby(data["sentiment"]==False)).mean()
+pos_rev_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==True)).mean()
+neg_rev_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==False)).mean()
+
