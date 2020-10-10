05479995e0313c1c8c65511b3829bf072916e003
Jorge <jsamo25@gmail.com>
2020-10-10 02:43:49 +0200

    Adding Histogram

--- Sentiment_classification.py
+++ Sentiment_classification.py
@@ -17,7 +17,7 @@ data["sentiment"] = data["sentiment"] == "pos"
 data["sentiment"] = data["sentiment"].astype("bool") #assign boolean values to sentiment column
 
 """PART J. Basic task and data explporation [max 2 points]"""
-#Report some basic statistics of the dataset (means, standard deviations). Also
+# 2. #Report some basic statistics of the dataset (means, standard deviations). Also
 # report the average number of words per review and the average number of sentences per review.
 
 data['n_characters'] = data['text'].apply(lambda x: len(x))
@@ -25,8 +25,12 @@ data["sentences"] = data["text"].apply(nltk.tokenize.sent_tokenize)
 data["n_sentences"] = data["sentences"].apply(lambda x: len(x))
 data['tokens'] = data['text'].apply(nltk.tokenize.word_tokenize)
 data['n_tokens'] = data['tokens'].apply(lambda x: len(x))
-
-#4
+# 3. Histogram of the rating column
+plt.hist(data["rating"])
+plt.title("Rating Histogram")
+plt.grid(True)
+plt.show()
+# 4. av. words/sentences for pos/neg reviews
 pos_rev_word_avg = (data["n_characters"].groupby(data["sentiment"]==True)).mean()
 neg_rev_word_avg = (data["n_characters"].groupby(data["sentiment"]==False)).mean()
 pos_rev_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==True)).mean()
