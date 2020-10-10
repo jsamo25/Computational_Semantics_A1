9db065d9a2cff0ee704cea31b5933e675deb9276
Jorge <jsamo25@gmail.com>
2020-10-10 17:18:28 +0200

    adding format update.

--- Sentiment_classification.py
+++ Sentiment_classification.py
@@ -10,39 +10,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from pdb import set_trace
 
-"""Importing the dataset + target column boolean transformation """
+""" Import dataset and extra files for sentiment analysis"""
+
 data = pd.read_csv("sentiment.csv")
 data["sentiment"] = data["sentiment"] == "pos"
 data["sentiment"] = data["sentiment"].astype("bool") #assign boolean values to sentiment column
 
-"""PART J. Basic task and data exploration [max 2 points]"""
-# 2. #Report some basic statistics of the dataset (means, standard deviations). Also
-# report the average number of words per review and the average number of sentences per review.
+# positive_lexicons = pd.read_csv("positive-words.csv").transpose().values[0]
+# negative_lexicons = pd.read_csv("negative-words.csv").transpose().values[0]
 
-data['n_characters'] = data['text'].apply(lambda x: len(x))
-data["sentences"] = data["text"].apply(nltk.tokenize.sent_tokenize)
-data["n_sentences"] = data["sentences"].apply(lambda x: len(x))
-data['tokens'] = data['text'].apply(nltk.tokenize.word_tokenize)
-data['n_tokens'] = data['tokens'].apply(lambda x: len(x))
-
-print(data.describe())
-
-# 3. Histogram of the rating column
-plt.hist(data["rating"])
-plt.title("Rating Histogram")
-plt.grid(True)
-#plt.show()
-
-# 4. av. words/sentences for pos/neg reviews
-pos_rev_word_avg = (data["n_characters"].groupby(data["sentiment"]==True)).mean()
-neg_rev_word_avg = (data["n_characters"].groupby(data["sentiment"]==False)).mean()
-pos_rev_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==True)).mean()
-neg_rev_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==False)).mean()
-
-""" PART K. Logistic regression with hand-chosen features"""
-#5 feature selection: # characters, # words, # sentences, # possitive words, # negative words (using ["rate"] is cheating)
-#TODO Add more lexicons to improve precision
-#TODO: perhaps taking the lexicons from a separate CSV file, dictionary, or similar.
 positive_lexicons = ["admire", "amazing", "assure", "celebration", "charm", "eager", "enthusiastic", "excellent",
                      "fancy", "fantastic", "frolic", "graceful", "happy", "joy", "luck", "majesty", "mercy", "nice",
                      "patience", "perfect", "proud", "rejoice", "relief", "respect", "satisfactorily", "sensational",
@@ -52,21 +28,52 @@ negative_lexicons = ["abominable", "anger", "anxious", "bad", "catastrophe", "ch
                      "hate", "idiot", "inflict", "lazy", "miserable", "mourn", "nervous", "objection", "pest", "plot",
                      "reject", "scream", "silly", "terrible", "unfriendly","vile", "wicked"]
 
-def positive_lex(tokens):
-    return sum(lexicon in positive_lexicons for lexicon in tokens)
-data['n_positive_lex'] = data['tokens'].apply(positive_lex)
-#print(data["n_positive_lex"])
+"""
+    PART J. Basic task and data exploration
+"""
+# 2. #Initial text analysis and basic statistics of the dataset.
 
-def negative_lex(tokens):
+data["sentences"]    = data["text"].apply(nltk.tokenize.sent_tokenize)
+data['tokens']       = data['text'].apply(nltk.tokenize.word_tokenize)
+data['n_characters'] = data['text'].apply(lambda x: len(x))
+data["n_sentences"]  = data["sentences"].apply(lambda x: len(x))
+data['n_tokens']     = data['tokens'].apply(lambda x: len(x))
+
+def count_positive_lexicons(tokens):
+    return sum(lexicon in positive_lexicons for lexicon in tokens)
+data['n_positive_lex'] = data['tokens'].apply(count_positive_lexicons)
+print(data["n_positive_lex"])
+
+def count_negative_lexicons(tokens):
     return sum(lexicon in negative_lexicons for lexicon in tokens)
-data['n_negative_lex'] = data['tokens'].apply(negative_lex)
-#print(data["n_negative_lex"])
+data['n_negative_lex'] = data['tokens'].apply(count_negative_lexicons)
+print(data["n_negative_lex"])
+print(data.describe())
+
+# 3. Histogram of the rating column
+plt.hist(data["rating"])
+plt.title("Rating Histogram")
+plt.grid(True)
+plt.show()
+
+# 4. av. words/sentences for pos/neg reviews
+positive_word_avg = (data["n_characters"].groupby(data["sentiment"]==True)).mean()
+negative_word_avg = (data["n_characters"].groupby(data["sentiment"]==False)).mean()
+positive_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==True)).mean()
+negative_sent_avg = (data["n_sentences"].groupby(data["sentiment"]==False)).mean()
+
+""" PART K. Logistic regression with hand-chosen features"""
+#5 feature selection: # characters, # words, # sentences, # possitive words, # negative words
+
+
 
 #6 Logistic regression
 
 data_train, data_test = train_test_split(data, test_size=0.2)
-y_train, y_test = data_train["sentiment"], data_test["sentiment"]
-x_train, x_test= data_train[["n_characters","n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]],data_test[["n_characters","n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]]
+y_train, y_test = data_train["sentiment"], \
+                   data_test["sentiment"]
+x_train, x_test= data_train[["n_characters","n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]],\
+                  data_test[["n_characters","n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]]
 model = LogisticRegression(max_iter=1000).fit(x_train,y_train)
 y_pred = model.predict(x_test)
 
@@ -113,7 +120,7 @@ np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
 plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=['Forged','Authorized'],
+plot_confusion_matrix(cnf_matrix, classes=['True','False'],
                       title='Confusion matrix, without normalization')
 
 plt.show()
