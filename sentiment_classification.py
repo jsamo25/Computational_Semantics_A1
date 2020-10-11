1fa2b8d6d2c7dad9a8dfff3d8e9192a0d69bef0c
Jorge <jsamo25@gmail.com>
2020-10-12 00:47:48 +0200

    Adding LogReg with CrossValidation to the models

--- sentiment_classification.py
+++ sentiment_classification.py
@@ -6,6 +6,7 @@ import itertools
 
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from pdb import set_trace
@@ -17,7 +18,7 @@ from pdb import set_trace
 
 data = pd.read_csv("sentiment.csv")
 data["sentiment"] = data["sentiment"] == "pos"
-data["sentiment"] = data["sentiment"].astype("bool")  # assign boolean values to sentiment column
+data["sentiment"] = data["sentiment"].astype("bool")
 
 positive_lexicons = ["admire", "amazing", "assure", "celebration", "charm", "eager", "enthusiastic", "excellent",
                      "fancy", "fantastic", "frolic", "graceful", "happy", "joy", "luck", "majesty", "mercy", "nice",
@@ -28,8 +29,8 @@ negative_lexicons = ["abominable", "anger", "anxious", "bad", "catastrophe", "ch
                      "hate", "idiot", "inflict", "lazy", "miserable", "mourn", "nervous", "objection", "pest", "plot",
                      "reject", "scream", "silly", "terrible", "unfriendly","vile", "wicked"]
 
-#A bigger list is provided from https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html
-#an 5% in accuracy is gained, however the processing time increases significantly
+# A bigger list is provided from https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html
+# aprox. 5% in accuracy is gained, however the processing time increases significantly
 # positive_lexicons = pd.read_csv("positive-words.csv").transpose().values[0]
 # negative_lexicons = pd.read_csv("negative-words.csv").transpose().values[0]
 
@@ -41,14 +42,16 @@ negative_lexicons = ["abominable", "anger", "anxious", "bad", "catastrophe", "ch
 
 # 2. #Initial text analysis and basic statistics of the dataset.
 data["sentences"] = data["text"].apply(nltk.tokenize.sent_tokenize)
-data["tokens"]    = data["text"].apply(nltk.tokenize.word_tokenize)
+data["tokens"] = data["text"].apply(nltk.tokenize.word_tokenize)
 data["n_characters"] = data["text"].apply(lambda x: len(x))
-data["n_tokens"]     = data["tokens"].apply(lambda x: len(x))
-data["n_sentences"]  = data["sentences"].apply(lambda x: len(x))
+data["n_tokens"] = data["tokens"].apply(lambda x: len(x))
+data["n_sentences"] = data["sentences"].apply(lambda x: len(x))
+
 
 def count_positive_lexicons(tokens):
     return sum(lexicon in positive_lexicons for lexicon in tokens)
 data["n_positive_lex"] = data["tokens"].apply(count_positive_lexicons)
+
 
 def count_negative_lexicons(tokens):
     return sum(lexicon in negative_lexicons for lexicon in tokens)
@@ -70,42 +73,61 @@ print("sentences average per sentiment\n", sent_avg_per_sentiment)
 print("word average per sentiment\n", word_avg_per_sentiment)
 
 
-
 """*********************************************************
     PART K. Logistic regression with hand-chosen features 
 *********************************************************"""
-
+print(
+    "\n ***************************************"
+    "\n    Using hand-chosen features as input"
+    "\n ***************************************"
+)
 
 # 5. using [rating] felt a bit of cheating so I removed that from my list.
-print("Selected features:"
-                        "\n [n_characters]"
-                        "\n [n_tokens]"
-                        "\n [n_sentences]"
-                        "\n [n_positive_lex]"
-                        "\n [n_negative_lex]")
+print(
+    "Selected features:"
+    "\n [n_characters]"
+    "\n [n_tokens]"
+    "\n [n_sentences]"
+    "\n [n_positive_lex]"
+    "\n [n_negative_lex]"
+)
 
 # 6. Logistic regression
 data_train, data_test = train_test_split(data, test_size=0.4)
 y_train, y_test = data_train["sentiment"], data_test["sentiment"]
 x_train, x_test = (
-                  data_train[["n_characters", "n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]],
-                   data_test[["n_characters", "n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]],)
+    data_train[
+        ["n_characters", "n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]
+    ],
+    data_test[
+        ["n_characters", "n_tokens", "n_sentences", "n_positive_lex", "n_negative_lex"]
+    ],
+)
 
 model = LogisticRegression(max_iter=1000).fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
-print("\nmodel score with [hand-chosen features]")
-print("training set score: ", model.score(x_train,y_train))
+print("\nInitial model score")
+print("training set score: ", model.score(x_train, y_train))
 print("testing set score:  ", model.score(x_test, y_test))
+
+# 13.Using LogisticRegressionCV
+print("\nChanging model to use CrossValidation...")
+model = LogisticRegressionCV(cv=10, random_state=0, max_iter=1000).fit(x_train,y_train)
+y_pred = model.predict(x_test)
+
+print("\nFinal model score [hand-chosen features] and [CV]")
+print("training set score: ", model.score(x_train, y_train))
+print("testing set score:  ", model.score(x_test, y_test))
+
 
 # 7. Evaluation Results || Compute confusion matrix
 # based on: https://towardsdatascience.com/demystifying-confusion-matrix-confusion-9e82201592fd
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title="Confusion matrix",
-                          cmap=plt.cm.Blues):
+def plot_confusion_matrix(
+    cm, classes, normalize=False, title="Confusion matrix", cmap=plt.cm.Blues
+):
 
-    print("Confusion matrix", "\nnormalization=",normalize )
+    print("Confusion matrix", "\nnormalization=", normalize)
     print(cm)
 
     plt.imshow(cm, interpolation="nearest", cmap=cmap)
@@ -118,7 +140,10 @@ def plot_confusion_matrix(cm, classes,
     fmt = ".2f" if normalize else "d"
     thresh = cm.max() / 2.0
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j,i,format(cm[i, j], fmt),
+        plt.text(
+            j,
+            i,
+            format(cm[i, j], fmt),
             horizontalalignment="center",
             color="white" if cm[i, j] > thresh else "black",
         )
@@ -127,6 +152,7 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel("Predicted label")
     plt.tight_layout()
 
+
 # Compute confusion matrix
 print("\n\n")
 cnf_matrix = confusion_matrix(y_test, y_pred)
@@ -134,96 +160,125 @@ np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
 plt.figure()
-plot_confusion_matrix(cnf_matrix,
-                      classes=["True", "False"],
-                      title="Confusion matrix, hand-chosen features")
+plot_confusion_matrix(
+    cnf_matrix,
+    classes=["True", "False"],
+    title="Confusion matrix",
+)
 plt.show()
 
 # extracting true_positives, false_positives, true_negatives, false_negatives
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 print(
-    "\n Using hand-chosen features",
-    "\n True Negatives: ", tn,
-    "\n True Positives: ", tp,
-    "\n False Positives: ", fp,
-    "\n False Negatives: ", fn
+    "\n True Negatives: ",tn,
+    "\n True Positives: ",tp,
+    "\n False Positives: ",fp,
+    "\n False Negatives: ",fn,
 )
 
 # Performance metrics when BOW feature is used
-accuracy  = (tn + tp) / (tp + tn + fp + fn)
+accuracy = (tn + tp) / (tp + tn + fp + fn)
 precision = tp / (tp + fp)
-recall    = tp / (tp + fn)
-f1_score  = (2 * precision * recall) / (precision + recall)
+recall = tp / (tp + fn)
+f1_score = (2 * precision * recall) / (precision + recall)
 
-print("\n From confusion matrix [hand-chosen features]:",
-      "\n Accuracy: ", round(accuracy,2)*100,
-      "\n Precision: ", round(precision,2)*100,
-      "\n Recall: ", round(recall,2)*100,
-      "\n F1 Score: ", round(f1_score)*100)
-
+print(
+    "\n Accuracy: ",
+    round(accuracy, 2) * 100,
+    "\n Precision: ",
+    round(precision, 2) * 100,
+    "\n Recall: ",
+    round(recall, 2) * 100,
+    "\n F1 Score: ",
+    round(f1_score) * 100,
+)
 
 
 """****************************************************************
-    Part L. Logistic regression with bad of words representations 
+    Part L. Logistic regression with bag of words representations 
 *****************************************************************"""
 
 
-print("\n *********************************"
-      "\n    Using BOW as feature input"
-      "\n *********************************")
+print(
+    "\n *********************************"
+    "\n    Using BOW as feature input"
+    "\n *********************************"
+)
 
-#8. computing Bag of Words
+# 8. computing Bag of Words
 vectorizer = CountVectorizer()
 vectorizer.fit(data_train["text"])
-x_train, x_test = vectorizer.transform(data_train["text"]), \
-                  vectorizer.transform(data_test["text"])
+x_train, x_test = vectorizer.transform(data_train["text"]), vectorizer.transform(
+    data_test["text"]
+)
 
 # 9. Fitting BOW into LR
 model = LogisticRegression(max_iter=1000, C=50).fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
 # 10a. verify if model is overfitting.
-print("\nmodel score with [BOW features]")
-print("training set score: ", model.score(x_train,y_train))
+print("\n Initial model score, using a weak regularization, C=50")
+print("training set score: ", model.score(x_train, y_train))
 print("testing set score:  ", model.score(x_test, y_test))
 
 # 10b. increase regularization to reduce overfitting
+print("\nadjusting regularization...")
 model = LogisticRegression(max_iter=1000, C=0.01).fit(x_train, y_train)
 y_pred = model.predict(x_test)
-print("\nscore after adjusting regularization")
-print("training set score: ", model.score(x_train,y_train))
+
+print("\nScore with stronger regularization, C=0.01")
+print("training set score: ", model.score(x_train, y_train))
 print("testing set score: ", model.score(x_test, y_test))
+
+# 13.Using LogisticRegressionCV and stronger regularization
+print("\nChanging LR model to use CrossValidation [BOW features]")
+model = LogisticRegressionCV(cv=10, random_state=0, max_iter=1000).fit(x_train,y_train)
+y_pred = model.predict(x_test)
+
+print("\nFinal model score [BOW features] and [CV]")
+print("training set score: ", model.score(x_train, y_train))
+print("testing set score:  ", model.score(x_test, y_test))
 
 # 11. Evaluation Results || Compute confusion matrix
 print("\n\n")
 cnf_matrix = confusion_matrix(y_test, y_pred)
 np.set_printoptions(precision=2)
 
-#Plot non-normalized confusion matrix
+# Plot non-normalized confusion matrix
 plt.figure()
-plot_confusion_matrix(cnf_matrix,
-                      classes=["True", "False"],
-                      title="Confusion matrix, BOW features")
+plot_confusion_matrix(
+    cnf_matrix, classes=["True", "False"], title="Confusion matrix"
+)
 plt.show()
 
 # extracting true_positives, false_positives, true_negatives, false_negatives
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 print(
-    "\n Using BOW as features",
-    "\n True Negatives: ", tn,
-    "\n True Positives: ", tp,
-    "\n False Positives: ", fp,
-    "\n False Negatives: ", fn
+    "\n True Negatives: ",tn,
+    "\n True Positives: ",tp,
+    "\n False Positives: ",fp,
+    "\n False Negatives: ",fn,
 )
 
 # Performance metrics when BOW feature is used
-accuracy  = (tn + tp) / (tp + tn + fp + fn)
+accuracy = (tn + tp) / (tp + tn + fp + fn)
 precision = tp / (tp + fp)
-recall    = tp / (tp + fn)
-f1_score  = (2 * precision * recall) / (precision + recall)
+recall = tp / (tp + fn)
+f1_score = (2 * precision * recall) / (precision + recall)
 
-print("\n From confusion matrix [BOW as features]:",
-      "\n Accuracy: ", round(accuracy,2)*100,
-      "\n Precision: ", round(precision,2)*100,
-      "\n Recall: ", round(recall,2)*100,
-      "\n F1 Score: ", round(f1_score)*100)
+print(
+    "\n Accuracy: ", round(accuracy, 2) * 100,
+    "\n Precision: ",round(precision, 2) * 100,
+    "\n Recall: ",round(recall, 2) * 100,
+    "\n F1 Score: ",round(f1_score) * 100,
+)
+
+
+"""****************************************************************
+    Part M. Format review & LR extra features. 
+*****************************************************************"""
+
+# 12. PEP 8 was automatically implemented after executing on terminal: $ black sentiment_classification.py
+#some recommendations were not really helping so where ignored.
+
+# 13. LogisticRegression with CV implemented.
